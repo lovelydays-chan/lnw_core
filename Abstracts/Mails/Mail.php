d481d2f6869f88da56b1a8b1cedd0d0550e7d334
Channarong Phonkhan <channarong.pho@ascendcorp.com>
2018-03-24 16:34:21 +0700

    test

---
+++ Abstracts/Mails/Mail.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Lnw\Core\Abstracts\Mails;
+
+use Zend\Mail as Mailable;
+
+/**
+ * Class Mail.
+ *
+ * @author  Mahmoud Zalt <mahmoud@zalt.me>
+ */
+
+abstract class Mail extends Mailable
+{
+
+}
