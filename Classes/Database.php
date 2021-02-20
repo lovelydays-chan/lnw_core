297bcc3c7966182b14417727292b0e325b9a686c
Channarong Phonkhan <>
2021-02-20 19:05:29 +0700

    test

--- Classes/Database.php
+++ Classes/Database.php
@@ -8,19 +8,19 @@ class Database
 {
     public function __construct()
     {
-        $config = require str_replace('//', '/', $_SERVER['DOCUMENT_ROOT'].'/'.(!empty(env('ROOT_PATH')) ? env('ROOT_PATH') : '').'/config/database.php');
+        $config = require str_replace('//', '/', $_SERVER['DOCUMENT_ROOT'] . '/config/database.php');
         $db_config = $config['connections'][$config['default']];
         $capsule = new Capsule();
         $capsule->addConnection([
-         'driver' => $db_config['driver'],
-         'host' => $db_config['host'],
-         'database' => $db_config['database'],
-         'username' => $db_config['username'],
-         'password' => $db_config['password'],
-         'charset' => $db_config['charset'],
-         'collation' => $db_config['collation'],
-         'prefix' => $db_config['prefix'],
-    ]);
+            'driver' => $db_config['driver'],
+            'host' => $db_config['host'],
+            'database' => $db_config['database'],
+            'username' => $db_config['username'],
+            'password' => $db_config['password'],
+            'charset' => $db_config['charset'],
+            'collation' => $db_config['collation'],
+            'prefix' => $db_config['prefix'],
+        ]);
         // Setup the Eloquent ORM…
         $capsule->bootEloquent();
     }
